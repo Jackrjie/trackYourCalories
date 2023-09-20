549f419088ac8cc763cd2225397bc018008229f2
jackrjie <jackrjie@gmail.com>
2023-09-20 21:44:12 +0800

    setup modules

---
+++ build.gradle.kts
@@ -0,0 +1,20 @@
+buildscript {
+    val kotlin_version by extra("1.9.20-Beta")
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath(Build.androidBuildTools)
+        classpath(Build.kotlinGradlePlugin)
+        classpath(Build.hiltAndroidGradlePlugin)
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
+
+        // NOTE: Do not place your application dependencies here; they belong
+        // in the individual module build.gradle files
+    }
+}
+
+tasks.register("clean", Delete::class){
+    delete(rootProject.buildDir)
+}

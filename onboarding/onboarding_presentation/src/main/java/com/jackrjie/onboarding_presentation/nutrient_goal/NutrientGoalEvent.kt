9f790492932fe2dbce8e7b3b9135b59f41293c45
jackrjie <jackrjie@gmail.com>
2023-09-27 02:24:25 +0800

    added NutrientGoalScreen

---
+++ onboarding/onboarding_presentation/src/main/java/com/jackrjie/onboarding_presentation/nutrient_goal/NutrientGoalEvent.kt
@@ -0,0 +1,12 @@
+package com.jackrjie.onboarding_presentation.nutrient_goal
+
+sealed class NutrientGoalEvent {
+
+    data class OnCarbRatioEnter(val ratio: String): NutrientGoalEvent()
+
+    data class OnProteinRatioEnter(val ratio: String): NutrientGoalEvent()
+
+    data class OnFatRatioEnter(val ratio: String): NutrientGoalEvent()
+
+    object OnNextClick: NutrientGoalEvent()
+}
